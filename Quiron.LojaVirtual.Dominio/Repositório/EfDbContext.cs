c7e05f04fc51e13e1b9350b2bc7e6f93c3a1c7e8
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-10-23 11:55:25 -0200

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositório/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositório
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Dominio/Repositório/ProdutoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositório
+{
+    public class ProdutoRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+        public IEnumerable<Produto> Produto
+        {
+            get { return _context.Produto; }
+        }
+    }
+}
