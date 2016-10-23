1e2f7e22b44ecf8bbf8534f64640551de1168338
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-10-23 14:45:30 -0200

    Aula 7 - Criação do controller vitrine

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Quiron.LojaVirtual.UnitTest
@@ -9,6 +10,24 @@ namespace Quiron.LojaVirtual.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void Take()
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            var resultado = from num in numeros.Take(5) select num;
+            int[] teste = { 1, 2, 3, 4, 5 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+
+        [TestMethod]
+        public void Skip()
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+            int[] teste = { 3, 4, 5 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
     }
 }
---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositório;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        public int ProdutosPorPagina = 3;
+        private ProdutoRepositorio _repositorio;
+        // GET: Vitrine
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            var produtos = _repositorio.Produto.OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+
+            return View(produtos);
+        }
+    }
+}
