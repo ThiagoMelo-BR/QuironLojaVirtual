eddbc7e216fd24a9eaaf755a1bd7c0b37eb3a58a
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-11-05 11:44:17 -0200

    Aula 16 - Adicionado a classe de teste do carrinho e colocado no primeiro método Adicionar item ao carrinho

---
+++ Quiron.LojaVirtual.UnitTest/TesteDeCarrinho.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using System.Linq;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class TesteDeCarrinho
+    {
+        //Teste de Adicionar Itens ao Carrinho
+        [TestMethod]
+        public void AdicionarItensaoCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+
+            Carrinho carrinho = new Carrinho();
+
+            carrinho.AdicionarItemCarrinho(produto1, 12);
+
+            carrinho.AdicionarItemCarrinho(produto2, 13);
+
+            carrinho.AdicionarItemCarrinho(produto2, 25);
+
+            ItemCarrinho[] itens = carrinho.ItensCarrinho.ToArray();
+
+            Assert.AreEqual(itens.Length, 2);
+        }
+    }
+}
