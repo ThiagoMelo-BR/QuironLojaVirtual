e3aec0a0884737a35fc52f63e9e251117f29d505
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-11-05 16:44:11 -0200

    Aulas 18 - Botão Adicionar ao carrinho 19 - Carrinho ViewModel

---
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositório;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Web.Models;
+
+
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutoRepositorio _repositorio;
+        // GET: Carrinho
+        public RedirectToRouteResult Adicionar(int produtoid, string returnUrl)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            Produto produto = _repositorio.Produto.FirstOrDefault(p => p.ProdutoId == produtoid);
+
+            if(produto != null)
+            {
+                ObterCarrinho().AdicionarItemCarrinho(produto, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            Carrinho carrinho = (Carrinho)Session["Session"];
+
+            if(carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Session"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            Produto produto = _repositorio.Produto.FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if(produto != null)
+            {
+                ObterCarrinho().RemoverItemCarrinho(produto);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CarrinhoViewModel
+            {
+                Carrinho = ObterCarrinho(),
+                ReturUrl = returnUrl
+            });
+        }
+    }
+}
