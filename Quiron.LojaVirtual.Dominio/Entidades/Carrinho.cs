e9e0640f1dc3761e1cb4285d99f37507d278f663
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-11-05 11:10:10 -0200

    Aula 15 - Criação da classe carrinho de compras

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Carrinho
+    {
+        private readonly List<ItemCarrinho> _itenscarrinho = new List<ItemCarrinho>();
+        //Adicionar
+        public void AdicionarItemCarrinho(Produto produto, decimal quantidade)
+        {
+            //Verifica se o item está inserido na lista, se não tiver devolve um objeto do tipo produto null
+            ItemCarrinho item = _itenscarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
+
+            if(item == null)
+            {
+                _itenscarrinho.Add(new ItemCarrinho
+                {
+                    Produto = produto,
+                    Quantidade = quantidade
+                });
+            }
+            else
+            {
+                item.Quantidade += quantidade;
+            }
+        }
+        //Remover item
+        public void RemoverItemCarrinho(Produto produto)
+        {
+            _itenscarrinho.RemoveAll(l => l.Produto.ProdutoId == produto.ProdutoId);
+        }
+        //Limpar o carrinho
+        public void LimparCarrinho()
+        {
+            _itenscarrinho.Clear();
+        }        
+        //ObterValorTotal
+        public Decimal ObterValorTotalCarrinho()
+        {
+            return _itenscarrinho.Sum(e => e.Produto.Preco * e.Quantidade);
+        }
+        //Itens do carrinho
+        public IEnumerable<ItemCarrinho> ItensCarrinho
+        {
+            get { return _itenscarrinho; }
+        }
+    }
+
+    public class ItemCarrinho
+    {
+        public Produto Produto { get; set; }
+
+        public decimal Quantidade { get; set; } 
+
+    }
+
+}
