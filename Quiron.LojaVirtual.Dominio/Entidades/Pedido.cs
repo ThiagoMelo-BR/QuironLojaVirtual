bfd9e66b29c7149fbc5a31c23256ac689485aa66
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-11-21 21:09:42 -0200

    Adicionado a classe Pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe o nome do cliente")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "Informe seu endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        [Display(Name ="Complemento")]
+        public string Complemento { get; set; }
+        
+        [Required(ErrorMessage = "Informe uma cidade")]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe o bairro")]
+        [Display(Name = "Bairro")]
+        public string Bairro { get; set; }
+
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Informe um e-mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Embrulha para presente?")]
+        public bool EmbrulhaParaPresente { get; set; }
+
+    }
+}
