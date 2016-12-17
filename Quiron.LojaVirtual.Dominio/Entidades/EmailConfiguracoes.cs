2ec1b7620cfe5225db3bf1a31e4cb8296060c390
Thiago Alves de Melo <thiago5423674@hotmail.com>
2016-12-17 16:33:33 -0200

    Classe Email Processar Pedido - Curso de ASP.NET MVC - Criando uma Loja Virtual - Aula 25

---
+++ Quiron.LojaVirtual.Dominio/Entidades/EmailConfiguracoes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class EmailConfiguracoes
+    {
+        public bool UsarSsl = false;
+        public string ServidorSmtp = "smtp.gmail.com";
+        public int ServidorPorta = 587;
+        public string Usuario = "tthiagoalvesmelo@gmail.com";
+        public string PastaArquivo = @"c:\EnvioEmail";
+        public bool EscreverArquivo = false;
+        public string De = "tthiagoalvesmelo@gmail.com";
+        public string Para = "thiago.melo@outlook.com.br";
+        public string Senha = "AdmSenac2129949";
+    }
+}
